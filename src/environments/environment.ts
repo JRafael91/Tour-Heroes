f3482a43f0b74e0dcb9dcee2df6e0000addf281f
JRafael91 <39107286+JRafael91@users.noreply.github.com>
2019-04-26 13:01:29 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyAVpWHB_SASitGZ9vbWWCFSpxxDKh7KSVY',
-    authDomain: 'tour-heroes-82ab2.firebaseapp.com',
-    databaseURL: 'https://tour-heroes-82ab2.firebaseio.com',
-    projectId: 'tour-heroes-82ab2',
-    storageBucket: 'tour-heroes-82ab2.appspot.com',
-    messagingSenderId: '277419091485'
-  }
+  production: false
 };
 
 /*
