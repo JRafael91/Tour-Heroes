8cf7bebf9fd1213c5585bc3e5b8839c6793bae8d
JRafael91 <rafa14494@gmail.com>
2019-04-26 12:59:51 -0700

    enviroments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAVpWHB_SASitGZ9vbWWCFSpxxDKh7KSVY',
+    authDomain: 'tour-heroes-82ab2.firebaseapp.com',
+    databaseURL: 'https://tour-heroes-82ab2.firebaseio.com',
+    projectId: 'tour-heroes-82ab2',
+    storageBucket: 'tour-heroes-82ab2.appspot.com',
+    messagingSenderId: '277419091485'
+  }
+};
